28ea915ee1533c1ff2b44f8067a4b6872e8d579d
Lana <gavrylovalova@gmail.com>
2023-02-28 10:46:02 -0800

    established ManyToMany relationship between guest and role

--- src/main/java/com/mywebnotebook/notebook/entity/Guest.java
+++ src/main/java/com/mywebnotebook/notebook/entity/Guest.java
@@ -6,6 +6,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -22,5 +25,10 @@ public class Guest {
     private String email;
     @Column(nullable = false)
     private String password;
+    @ManyToMany (fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name="guests_accessibility",
+            joinColumns = {@JoinColumn (name="guest_id", referencedColumnName = "id")},
+            inverseJoinColumns = {@JoinColumn(name="role_id", referencedColumnName = "id")})
+    private List<Role> roles = new ArrayList<>();
 
 }
